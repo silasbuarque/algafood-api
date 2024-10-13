5fe1c8af6ccea3f11374c711044a1931a0f89a85
Silas Buarque <silasbuarque@gmail.com>
2024-10-13 20:45:53 -0300

    Initial Commit

--- algafood-api/src/main/java/com/buarque/algafood/domais/exception/EntidadeEmUsoException.java
+++
@@ -1,8 +0,0 @@
-package com.buarque.algafood.domais.exception;
-
-public class EntidadeEmUsoException extends RuntimeException {
-
-    public EntidadeEmUsoException(String message) {
-        super(message);
-    }
-}
--- algafood-api/src/main/java/com/buarque/algafood/domais/exception/EntidadeNaoEncontradaException.java
+++
@@ -1,7 +0,0 @@
-package com.buarque.algafood.domais.exception;
-
-public class EntidadeNaoEncontradaException extends RuntimeException {
-    public EntidadeNaoEncontradaException(String message) {
-        super(message);
-    }
-}
