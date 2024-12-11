9646aea71c4822c32226a99fe001f26d7b7dbe9b
Silas Buarque <silasbuarque@gmail.com>
2024-12-11 07:26:13 -0300

    Fazendo CRUD utilizando EntityManager

---
+++ src/main/java/com/algafood/algafoodapi/jpa/AlteracaoCozinhaMain.java
@@ -0,0 +1,29 @@
+package com.algafood.algafoodapi.jpa;
+
+import com.algafood.algafoodapi.AlgafoodApiApplication;
+import com.algafood.algafoodapi.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+public class AlteracaoCozinhaMain {
+
+    public static void main(String[] args) {
+
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+        Cozinha cozinha = new Cozinha();
+        cozinha.setId(1L);
+        cozinha.setNome("Brasileira");
+
+        cozinha = cadastroCozinha.salvar(cozinha);
+
+        System.out.println(cozinha.getNome());
+
+    }
+
+}
--- src/main/java/com/algafood/algafoodapi/jpa/CadastroCozinha.java
+++ src/main/java/com/algafood/algafoodapi/jpa/CadastroCozinha.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 import com.algafood.algafoodapi.domain.model.Cozinha;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
 
 @Component
 public class CadastroCozinha {
@@ -19,4 +19,18 @@ public class CadastroCozinha {
         return manager.createQuery("from Cozinha", Cozinha.class).getResultList();
     }
 
+    public Cozinha buscar(Long id) {
+        return manager.find(Cozinha.class, id);
+    }
+
+    @Transactional
+    public Cozinha salvar(Cozinha cozinha) {
+        return manager.merge(cozinha);
+    }
+
+    @Transactional
+    public void remover(Cozinha cozinha) {
+        cozinha = buscar(cozinha.getId());
+        manager.remove(cozinha);
+    }
 }
