28f86550dd0ca60dcb13517f3fe1d7435a57e89e
Silas Buarque <silasbuarque@gmail.com>
2025-01-03 09:13:39 -0300

    Entendendo o ResponseEntity e fazendo o endpoint de salvar cozinha

--- src/main/java/com/algafood/algafoodapi/api/controller/CozinhaController.java
+++ src/main/java/com/algafood/algafoodapi/api/controller/CozinhaController.java
@@ -4,11 +4,11 @@ import com.algafood.algafoodapi.api.model.CozinhasXmlWrapper;
 import com.algafood.algafoodapi.domain.model.Cozinha;
 import com.algafood.algafoodapi.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,8 +30,20 @@ public class CozinhaController {
     }
 
     @GetMapping("/{cozinhaId}")
-    public Cozinha buscar(@PathVariable Long cozinhaId) {
-        return cozinhaRepository.buscar(cozinhaId);
+    public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+        Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+
+        if (cozinha != null) {
+            return ResponseEntity.ok(cozinha);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cozinha adicionar(@RequestBody Cozinha cozinha) {
+        return cozinhaRepository.salvar(cozinha);
     }
 
 }
--- src/main/java/com/algafood/algafoodapi/domain/model/Cozinha.java
+++ src/main/java/com/algafood/algafoodapi/domain/model/Cozinha.java
@@ -1,15 +1,11 @@
 package com.algafood.algafoodapi.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
+
 import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.Objects;
 
 @JsonRootName("cozinha")
 @Data
