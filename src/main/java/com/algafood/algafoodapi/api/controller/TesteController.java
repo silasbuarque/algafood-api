114a24a052abe1ab4e80b036bf7bb2f5dd3ddf1e
Silas Buarque <silasbuarque@gmail.com>
2025-01-05 13:50:25 -0300

    Usando JPQL nas consultas

---
+++ src/main/java/com/algafood/algafoodapi/api/controller/TesteController.java
@@ -0,0 +1,25 @@
+package com.algafood.algafoodapi.api.controller;
+
+import com.algafood.algafoodapi.domain.model.Cozinha;
+import com.algafood.algafoodapi.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/teste")
+public class TesteController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping("/cozinhas/por-nome")
+    public List<Cozinha> porNome(@RequestParam String nome) {
+        return cozinhaRepository.consultarPorNome(nome);
+    }
+
+}
--- src/main/java/com/algafood/algafoodapi/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algafood/algafoodapi/domain/repository/CozinhaRepository.java
@@ -1,12 +1,15 @@
 package com.algafood.algafoodapi.domain.repository;
 
 import com.algafood.algafoodapi.domain.model.Cozinha;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public interface CozinhaRepository {
 
     List<Cozinha> listar();
+    List<Cozinha> consultarPorNome(String nome);
     Cozinha buscar(Long id);
     Cozinha salvar(Cozinha cozinha);
     void remover(Long id);
