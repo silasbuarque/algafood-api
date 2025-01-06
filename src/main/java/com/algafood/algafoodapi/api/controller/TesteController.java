5a680c831cff43e4818b2a30209e701d15fee0a0
Silas Buarque <silasbuarque@gmail.com>
2025-01-06 05:54:53 -0300

    Testando query Method do JPA e atualizando a versão do JPA

--- src/main/java/com/algafood/algafoodapi/api/controller/TesteController.java
+++ src/main/java/com/algafood/algafoodapi/api/controller/TesteController.java
@@ -1,14 +1,18 @@
 package com.algafood.algafoodapi.api.controller;
 
 import com.algafood.algafoodapi.domain.model.Cozinha;
+import com.algafood.algafoodapi.domain.model.Restaurante;
 import com.algafood.algafoodapi.domain.repository.CozinhaRepository;
+import com.algafood.algafoodapi.domain.repository.RestauranteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/teste")
@@ -17,14 +21,27 @@ public class TesteController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+
     @GetMapping("/cozinhas/por-nome")
     public List<Cozinha> porNome(@RequestParam String nome) {
-        return cozinhaRepository.findTodasByNome(nome);
+        return cozinhaRepository.findTodasByNomeContaining(nome);
     }
 
     @GetMapping("/cozinhas/unica-por-nome")
-    public Cozinha unicaPorNome(@RequestParam String nome) {
+    public Optional<Cozinha> unicaPorNome(@RequestParam String nome) {
         return cozinhaRepository.findByNome(nome);
+    }
+
+    @GetMapping("/restaurantes/por-taxa")
+    public List<Restaurante> restaurantesTaxa(BigDecimal taxaInicial, BigDecimal taxaFinal) {
+        return restauranteRepository.findByTaxaFreteBetween(taxaInicial, taxaFinal);
+    }
+
+    @GetMapping("/restaurantes/por-cozinha")
+    public List<Restaurante> restaurantesTaxa(String nome, Long cozinhaId) {
+        return restauranteRepository.findByNomeContainingAndCozinhaId(nome, cozinhaId);
     }
 
 }
--- src/main/java/com/algafood/algafoodapi/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algafood/algafoodapi/domain/repository/CozinhaRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 
-    List<Cozinha> findTodasByNome(String nome);
-    Cozinha findByNome(String nome);
+    List<Cozinha> findTodasByNomeContaining(String nome);
+    Optional<Cozinha> findByNome(String nome);
 }
