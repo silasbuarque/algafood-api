8e55619a5fb09a5562807b1b32d85896c85efddf
Silas Buarque <silasbuarque@gmail.com>
2025-01-15 08:52:15 -0300

    Resolvendo os desafios (6.7), (6.8) e (6.9);

--- src/main/java/com/algafood/algafoodapi/domain/model/Restaurante.java
+++ src/main/java/com/algafood/algafoodapi/domain/model/Restaurante.java
@@ -67,4 +67,7 @@ public class Restaurante {
                 inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
     private List<FormaPagamento> formasPagamento = new ArrayList<>();
 
+    @OneToMany(mappedBy = "restaurante")
+    private List<Produto> produtos = new ArrayList<>();
+
 }
