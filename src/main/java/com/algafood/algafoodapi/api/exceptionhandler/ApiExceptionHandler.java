d1fdd8e950f6f3910e006c5441ff8c587d1ad21c
Silas Buarque <silasbuarque@gmail.com>
2025-01-23 07:36:59 -0300

    Criando um exception handler global com ResponseEntityExceptionHandler

--- src/main/java/com/algafood/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algafood/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -5,16 +5,18 @@ import com.algafood.algafoodapi.domain.exception.EntidadeNaoEncontradaException;
 import com.algafood.algafoodapi.domain.exception.NegocioException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
-@ControllerAdvice // Estamos utilizando para fazer com que o tratamento seja global (toda API)
-public class ApiExceptionHandler {
-
+/**
+ * Estamos utilizando @ControllerAdvice para fazer com que o tratamento seja global (toda API)
+ * ResponseEntityExceptionHandler é responsável por tratar todas as exceptions do spring
+ */
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
     public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e) {
@@ -46,13 +48,4 @@ public class ApiExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
     }
 
-    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-    public ResponseEntity<?> tratarHttpMediaTypeNotSupportedException() {
-        Problema problema = Problema.builder()
-                .dataHora(LocalDateTime.now())
-                .menssagem("Tipo de mídia não suportado.")
-                .build();
-
-        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(problema);
-    }
 }
