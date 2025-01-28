904e547f68000e8f359888ebcc9339c2a60924be
Silas Buarque <silasbuarque@gmail.com>
2025-01-28 18:13:21 -0300

    Desafio: tratando a exception customizada de validações programáticas

--- src/main/java/com/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.algafoodapi.api.exceptionhandler;
 
+import com.algafoodapi.core.validation.ValidacaoException;
 import com.algafoodapi.domain.exception.EntidadeEmUsoException;
 import com.algafoodapi.domain.exception.EntidadeNaoEncontradaException;
 import com.algafoodapi.domain.exception.NegocioException;
@@ -42,30 +43,37 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @Autowired
     private MessageSource messageSource;
 
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    @ExceptionHandler({ ValidacaoException.class })
+    public ResponseEntity<Object> handleValidacaoException(ValidacaoException ex, WebRequest request) {
+        return handleValidationInternal(ex, ex.getBindingResult(), new HttpHeaders(),
+                HttpStatus.BAD_REQUEST, request);
+    }
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return handleValidationInternal(ex, ex.getBindingResult(), headers, status, request);
+    }
+
+    private ResponseEntity<Object> handleValidationInternal(Exception ex, BindingResult bindingResult, HttpHeaders headers,
+                                                            HttpStatus status, WebRequest request) {
         ProblemType problemType = ProblemType.DADOS_INVALIDOS;
         String detail = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.";
 
-        BindingResult bindingResult = ex.getBindingResult();
-
         List<Problem.Object> problemObjects = bindingResult.getAllErrors().stream()
-                .map(objectError ->  {
+                .map(objectError -> {
+                    String message = messageSource.getMessage(objectError, LocaleContextHolder.getLocale());
 
-                        String message = messageSource.getMessage(objectError, LocaleContextHolder.getLocale());
+                    String name = objectError.getObjectName();
 
-                        String name = objectError.getObjectName();
+                    if (objectError instanceof FieldError) {
+                        name = ((FieldError) objectError).getField();
+                    }
 
-                        if (objectError instanceof FieldError) {
-                            name = ((FieldError) objectError).getField();
-                        }
-
-                        return Problem.Object.builder()
+                    return Problem.Object.builder()
                             .name(name)
                             .userMessage(message)
                             .build();
-
                 })
                 .collect(Collectors.toList());
 
@@ -76,6 +84,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
         return handleExceptionInternal(ex, problem, headers, status, request);
     }
+
 
     @Override
     protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
