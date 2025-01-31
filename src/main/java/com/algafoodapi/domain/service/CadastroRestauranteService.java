30e9c5d65c02846ba5835e06461d8dbe2c9943c9
Silas Buarque <silasbuarque@gmail.com>
2025-01-31 15:02:41 -0300

    Analisando e definindo melhor o escopo das transações

--- src/main/java/com/algafoodapi/domain/service/CadastroRestauranteService.java
+++ src/main/java/com/algafoodapi/domain/service/CadastroRestauranteService.java
@@ -6,6 +6,7 @@ import com.algafoodapi.domain.model.Restaurante;
 import com.algafoodapi.domain.repository.RestauranteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CadastroRestauranteService {
@@ -16,6 +17,7 @@ public class CadastroRestauranteService {
     @Autowired
     private CadastroCozinhaService cozinhaService;
 
+    @Transactional
     public Restaurante salvar(Restaurante restaurante) {
 
         Long cozinhaId = restaurante.getCozinha().getId();
