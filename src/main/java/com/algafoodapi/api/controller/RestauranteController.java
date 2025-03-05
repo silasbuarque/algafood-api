b87803c17314778404c6fd2b040c64f416d13305
Silas Buarque <silasbuarque@gmail.com>
2025-03-05 11:31:12 -0300

    Fazendo projeção de recursos com @JsonView do Jackson

--- src/main/java/com/algafoodapi/api/controller/RestauranteController.java
+++ src/main/java/com/algafoodapi/api/controller/RestauranteController.java
@@ -8,12 +8,14 @@ import com.algafoodapi.api.assembler.RestauranteDTOAssembler;
 import com.algafoodapi.api.assembler.RestauranteInputDisassembler;
 import com.algafoodapi.api.model.RestauranteDTO;
 import com.algafoodapi.api.model.input.RestauranteInput;
+import com.algafoodapi.api.model.view.RestauranteView;
 import com.algafoodapi.domain.exception.CozinhaNaoEncontradaException;
 import com.algafoodapi.domain.exception.NegocioException;
 import com.algafoodapi.domain.exception.RestauranteNaoEncontradoException;
 import com.algafoodapi.domain.model.Restaurante;
 import com.algafoodapi.domain.repository.RestauranteRepository;
 import com.algafoodapi.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.annotation.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -34,10 +36,53 @@ public class RestauranteController {
     @Autowired
     private RestauranteInputDisassembler restauranteInputDisassembler;
 
+    @JsonView(RestauranteView.Resumo.class)
     @GetMapping
     public List<RestauranteDTO> listar() {
         return restauranteDTOAssembler.toCollectionsModel(restauranteRepository.findAll());
     }
+
+    @JsonView(RestauranteView.ApenasNome.class)
+    @GetMapping(params = "projecao=apenas-nome")
+    public List<RestauranteDTO> listarApenasNomes() {
+        return listar();
+    }
+
+//	@GetMapping
+//	public MappingJacksonValue listar(@RequestParam(required = false) String projecao) {
+//		List<Restaurante> restaurantes = restauranteRepository.findAll();
+//		List<RestauranteDTO> restaurantesModel = restauranteDTOAssembler.toCollectionsModel(restaurantes);
+//
+//		MappingJacksonValue restaurantesWrapper = new MappingJacksonValue(restaurantesModel);
+//
+//		restaurantesWrapper.setSerializationView(RestauranteView.Resumo.class);
+//
+//		if ("apenas-nome".equals(projecao)) {
+//			restaurantesWrapper.setSerializationView(RestauranteView.ApenasNome.class);
+//		} else if ("completo".equals(projecao)) {
+//			restaurantesWrapper.setSerializationView(null);
+//		}
+//
+//		return restaurantesWrapper;
+//	}
+//
+//	@GetMapping
+//	public List<RestauranteDTO> listar() {
+//		return restauranteDTOAssembler.toCollectionsModel(restauranteRepository.findAll());
+//	}
+//
+//	@JsonView(RestauranteView.Resumo.class)
+//	@GetMapping(params = "projecao=resumo")
+//	public List<RestauranteDTO> listarResumido() {
+//		return listar();
+//	}
+//
+//	@JsonView(RestauranteView.ApenasNome.class)
+//	@GetMapping(params = "projecao=apenas-nome")
+//	public List<RestauranteDTO> listarApenasNomes() {
+//		return listar();
+//	}
+
 
     @GetMapping("/{restauranteId}")
     public RestauranteDTO buscar(@PathVariable Long restauranteId) {
---
+++ src/main/java/com/algafoodapi/api/model/view/RestauranteView.java
@@ -0,0 +1,11 @@
+package com.algafoodapi.api.model.view;
+
+public interface RestauranteView {
+
+    public interface Resumo {
+    }
+
+    public interface ApenasNome {
+    }
+
+}
